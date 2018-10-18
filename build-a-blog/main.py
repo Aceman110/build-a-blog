e9851d954b9d015baa06a041cfca5cf3190c9e45
Aceman110 <43358556+Aceman110@users.noreply.github.com>
2018-10-18 17:37:10 -0500

    Update main.py

--- build-a-blog/main.py
+++ build-a-blog/main.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, redirect, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -11,9 +10,6 @@ db = SQLAlchemy(app)
 
 
 class Entry(db.Model):
-    '''
-    Stores blog entries
-    '''
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(180))
     body = db.Column(db.String(1000))
@@ -36,18 +32,10 @@ class Entry(db.Model):
 
 @app.route("/")
 def index():
-    '''
-    Convenience route so the bare URL displays all the entries
-    '''
     return redirect("/blog")
 
 @app.route("/blog")
 def display_blog_entries():
-    '''
-    Either list one entry with the given ID
-    Or list all blog entries (in default or newest order)
-    '''
-    
     entry_id = request.args.get('id')
     if (entry_id):
         entry = Entry.query.get(entry_id)
@@ -63,10 +51,6 @@ def display_blog_entries():
 
 @app.route('/new_entry', methods=['GET', 'POST'])
 def new_entry():
-    '''
-    GET: Display form for new blog entry
-    POST: create new entry or redisplay form if values are invalid
-    '''
     if request.method == 'POST':
         new_entry_title = request.form['title']
         new_entry_body = request.form['body']
@@ -75,8 +59,6 @@ def new_entry():
         if new_entry.is_valid():
             db.session.add(new_entry)
             db.session.commit()
-
-            
             url = "/blog?id=" + str(new_entry.id)
             return redirect(url)
         else:
